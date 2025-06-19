bc838b1edd83e4bf9f267ea1578bdde73a346ac8
Mahesh-Banduni <maheshbanduni9997@gmail.com>
2025-06-19 21:06:17 +0530

    added code

--- client/src/app/auth/signin/page.jsx
+++ client/src/app/auth/signin/page.jsx
@@ -41,7 +41,7 @@ export default function SignIn() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gray-700">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold mb-6 text-center">
           {isLogin ? 'Sign In' : 'Sign Up'}
